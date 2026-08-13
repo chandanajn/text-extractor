8ee56d2460e081113fb02dc32211fc4392b584e3
chandanajn <jnchandana2003@gmail.com>
2026-08-14 00:14:22 +0530

    fix5: adding sonarqube

--- backend/core/config.py
+++ backend/core/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
 
-
     # Database
     POSTGRES_SERVER: str = "localhost"
     POSTGRES_USER: str = "postgres"
@@ -34,4 +33,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
